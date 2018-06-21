12624e1d535d9137697beaf5af0f06d0c013fc2d
wattsian6 <watts.ian6@gmail.com>
2018-06-21 14:13:30 -0400

    Now splits audio into 30 sec intervals

--- stitcher.py
+++ stitcher.py
@@ -9,18 +9,24 @@ directory = os.fsencode(path)
 outfile = 'C:\\Users\\wattsij\\Documents\\audio_combiner\\output.wav'
 num_of_files = 20                       #3166
 samples_per_file = 441344
-out = np.zeros((num_of_files * samples_per_file,), dtype=np.int16)
+out = np.zeros((3 * samples_per_file,), dtype=np.int16)
 i = 0
-
-
+k = 0
+files_processed = 0
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     if filename.endswith(".wav"):
+
+
         path_file = path + '\\' + filename                  #
         rate, data = scipy.io.wavfile.read(path_file)       #READS RATE AND DATA ARRAY
         for j in range(samples_per_file):
             out[(i * samples_per_file) + j] = data[j]
         i += 1
+        files_processed += 1
 
-print(out.shape)
-scipy.io.wavfile.write("output.wav", rate, out)
+        if (((i % 3) == 0) or (num_of_files == files_processed)):
+            scipy.io.wavfile.write("output" + str(k) + ".wav", rate, out)
+            out = np.zeros((3 * samples_per_file,), dtype=np.int16)
+            k += 1
+            i = 0
